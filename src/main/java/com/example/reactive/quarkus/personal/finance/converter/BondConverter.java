0aa984ad9c89385163995be562e30b896dffcbbb
fabianaspeeencina <faspeeencina@gmail.com>
2025-04-10 20:28:49 +0200

    added crud operation
    added operation to doing crud operation on bond entity, added repository, service and controller and converter

--- src/main/java/com/example/reactive/quarkus/personal/finance/converter/BondConverter.java
+++ src/main/java/com/example/reactive/quarkus/personal/finance/converter/BondConverter.java
@@ -1,22 +1,38 @@
 package com.example.reactive.quarkus.personal.finance.converter;
 
 import com.example.reactive.quarkus.personal.finance.model.entity.Bond;
+import com.example.reactive.quarkus.personal.finance.model.entity.User;
 import com.example.reactive.quarkus.personal.finance.model.request.BondRequestDto;
 import com.example.reactive.quarkus.personal.finance.model.response.BondResponseDto;
+
+import java.util.UUID;
 
 public class BondConverter implements Converter<BondRequestDto, BondResponseDto, Bond> {
     @Override
     public BondResponseDto toDto(Bond entity) {
-        return null;
+        return new BondResponseDto(entity.getId().toString(), entity.getUser().getId().toString(), entity.getName(),
+                entity.getCode(), entity.getInvestedAmount(), entity.getAnnualRate(), entity.getMaturityDate(),
+                entity.getCouponType(), entity.getCurrentValue());
     }
 
     @Override
     public Bond toEntity(BondRequestDto dto) {
-        return null;
+        Bond bond = new Bond();
+        bond.setCode(dto.code());
+        bond.setName(dto.name());
+        bond.setInvestedAmount(dto.investedAmount());
+        bond.setAnnualRate(dto.annualRate());
+        bond.setMaturityDate(dto.maturityDate());
+        bond.setCouponType(dto.couponType());
+        bond.setCurrentValue(dto.currentValue());
+        User user = new User();
+        user.setId(UUID.fromString(dto.userId()));
+        bond.setUser(user);
+        return bond;
     }
 
     @Override
     public Bond toEntity(Long id, BondRequestDto dto) {
-        return null;
+        return toEntity(dto);
     }
 }
